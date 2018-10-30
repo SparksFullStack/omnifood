2298b509f8af8f9a44ac9c85b2f43be5f890dd7c
SparksFullStack <csparks@antioch.edu>
2018-10-30 12:48:43 -0700

    sticky menu set to display once a down scroll starts

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -1,3 +1,14 @@
 $(document).ready(function(){
-    
+
+    // adding sticky navigation
+    $('.js--section-features').waypoint(function(direction) {
+        if (direction == 'down'){
+            $('nav').addClass('sticky')
+        } else {
+            $('nav').removeClass('sticky');
+        }
+    }, {
+        offset: '93%'
+    });
+
 });
